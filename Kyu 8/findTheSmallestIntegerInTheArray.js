c3d0c6318c2c7d1a1d2703e461c5823180727c29
cadinkor <elliott.thomlison@gmail.com>
2021-03-04 03:48:26 +0000

    reviewing, readjusting, refactoring

---
+++ Kyu 8/findTheSmallestIntegerInTheArray.js
@@ -0,0 +1,19 @@
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    let smallest = Infinity 
+    for(let i = 0; i < args.length; i++){
+      if (args[i] < smallest){
+        smallest = args[i]
+      }
+    }
+    return smallest
+  }
+}
+
+ //or 
+
+ class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
